453d2d7076209e7be8a097c8ec6b6d5fc453cf5b
Karetski <karetski@gmail.com>
2016-08-06 23:10:33 +0300

    Test target added

---
+++ SnowonderTests/SnowonderTests.swift
@@ -0,0 +1,27 @@
+//
+//  SnowonderTests.swift
+//  SnowonderTests
+//
+//  Created by Alexey on 06.08.16.
+//  Copyright © 2016 Aliaksei Karetski. All rights reserved.
+//
+
+import XCTest
+
+class SnowonderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(true)
+    }
+    
+}
