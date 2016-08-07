6436105174e7f44f8c59e9ebcf3eb7eee7aa2541
Karetski <karetski@gmail.com>
2016-08-07 22:09:03 +0300

    IDE extensions removed from compile sources

--- Snowonder/General/Common/Extensions/IDESourceCodeDocument+sourceCodeType.swift
+++ Snowonder/General/Common/Extensions/IDESourceCodeDocument+sourceCodeType.swift
@@ -6,27 +6,27 @@
 //  Copyright © 2016 Alexey Karetski. All rights reserved.
 //
 
-//extension IDESourceCodeDocument {
-//    
-//    enum Type {
-//        case Undefined
-//        case Swift
-//        case ObjectiveC
-//    }
-//    
-//    func type() -> Type {
-//        var sourceCodeType: Type = .Undefined
-//        
-//        guard let fileURL = self.fileURL, let pathExtension = fileURL.pathExtension else {
-//            return sourceCodeType
-//        }
-//        
-//        if ["swift"].contains(pathExtension) {
-//            sourceCodeType = .Swift
-//        } else if ["h", "m", "mm"].contains(pathExtension) {
-//            sourceCodeType = .ObjectiveC
-//        }
-//        
-//        return sourceCodeType
-//    }
-//}
+extension IDESourceCodeDocument {
+    
+    enum Type {
+        case Undefined
+        case Swift
+        case ObjectiveC
+    }
+    
+    func type() -> Type {
+        var sourceCodeType: Type = .Undefined
+        
+        guard let fileURL = self.fileURL, let pathExtension = fileURL.pathExtension else {
+            return sourceCodeType
+        }
+        
+        if ["swift"].contains(pathExtension) {
+            sourceCodeType = .Swift
+        } else if ["h", "m", "mm"].contains(pathExtension) {
+            sourceCodeType = .ObjectiveC
+        }
+        
+        return sourceCodeType
+    }
+}
