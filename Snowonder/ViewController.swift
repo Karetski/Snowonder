e173736543ad4c66e4a54cabb5cf41f44fb7f57b
Karetski <karetski@gmail.com>
2017-06-15 14:02:35 +0300

    Setup extension project

---
+++ Snowonder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Snowonder
+//
+//  Created by Aliaksei Karetski on 15.06.17.
+//  Copyright © 2017 Karetski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
