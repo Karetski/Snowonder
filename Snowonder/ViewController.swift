d5b1ef4caf28e2ab5a69ebd2459b0ca5ffb09f21
Karetski <karetski@gmail.com>
2017-07-07 12:43:02 +0300

    Enable script added

--- Snowonder/ViewController.swift
+++ Snowonder/ViewController.swift
@@ -9,16 +9,26 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    // MARK: - Constant values
+    
+    private struct Constant {
+        
+        struct ScriptInfo {
+            
+            static let openSystemPreferencesExtensions: Script.Info = (Bundle.main, "open_system_preferences_extensions", .scpt)
+        }
+    }
+    
+    // MARK: - Controller lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    
+    // MARK: - Action handlers
+    
+    @IBAction func enableExtensionButtonAction(_ sender: NSButton) {
+        Script(info: Constant.ScriptInfo.openSystemPreferencesExtensions).execute()
     }
 }
